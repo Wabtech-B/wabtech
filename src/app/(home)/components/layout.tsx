acae384878e1fe2ef3d58cb1c7b5d3c354240bb0
Wabweni Brian <115115387+WabweniBrian@users.noreply.github.com>
2023-08-29 13:48:12 +0300

    Updates

---
+++ src/app/(home)/components/layout.tsx
@@ -0,0 +1,29 @@
+import ComponentSearchDialog from "@/components/common/ComponentSearchDialog";
+import ComponentSidebar from "@/components/common/ComponentSidebar";
+import MobileComponentSidebar from "@/components/common/MobileComponentSidebar";
+import Navbar from "@/components/common/Navbar";
+
+const Component = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <MobileComponentSidebar />
+      <ComponentSearchDialog />
+      <div
+        className="bg-image"
+        style={{ background: `url('/bg-pattern.png')` }}
+      />
+      <div className="bg-white/60 dark:bg-zinc-900/60 backdrop-blur-[130px] dark:backdrop-blur-[100px] fixed w-full h-full -z-10" />
+      <div className="grid grid-cols-1 lg:grid-cols-250-auto">
+        {/* Sidebar */}
+        <div>
+          <ComponentSidebar />
+        </div>
+
+        {/* Main Content */}
+        <div className="p-4 pt-24">{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default Component;
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,20 +2,18 @@
 import { FiArrowRight, FiMonitor, FiMoon, FiSun } from "react-icons/fi";
 import { BiSearchAlt } from "react-icons/bi";
 import { useRef } from "react";
-import { FiMenu } from "react-icons/fi";
 import { useState } from "react";
 import Dropdown from "@/components/wabtech/dropdown/Dropdown";
-import Drawer from "@/components/wabtech/drawer/Drawer";
 import Button from "@/components/wabtech/button/Button";
 import useDarkMode from "@/components/wabtech/hooks/useDarkmode/useDarkMode";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   const { mode, handleModeChange } = useDarkMode("darkModePreference");
-  const [isDrawerOpen, setisDrawerOpen] = useState(false);
   const [isNavbarMenuOpen, setIsNavbarMenuOpen] = useState(false);
   const pathname = usePathname();
   const commandButtonRef = useRef(null);
@@ -58,17 +56,29 @@ const Navbar = () => {
   return (
     <nav className="fixed w-full top-0 left-0 bg-white/80 border-b dark:border-b-zinc-800 dark:bg-zinc-900/80 backdrop-blur-sm px-4 md:px-8 h-14 z-20">
       <div className="flex-center-between">
-        <Link
-          href="/components/getting-started"
-          className="my-4 flex-align-center"
-        >
+        <Link href="/" className="my-4 flex-align-center">
           <Image src="/logo.png" alt="" width={40} height={40} />
           <h1 className="text-2xl font-bold">Wabtech</h1>
         </Link>
         <div className="flex-align-center gap-x-10">
           <ul className="hidden md:flex ml-6 space-x-4 navbar-links">
             {navbarLinks.map((link) => {
-              const isActive = pathname === link.url;
+              let isActive = pathname === link.url; // Check for exact match first
+
+              if (!isActive) {
+                if (
+                  link.url.startsWith("/components") &&
+                  pathname.startsWith("/components")
+                ) {
+                  isActive = true; // Check for "/components" and its sub-routes
+                } else if (
+                  pathname.startsWith(link.url) &&
+                  link.url !== "/components"
+                ) {
+                  isActive = true; // Check for other links and their sub-routes
+                }
+              }
+
               return (
                 <li key={link.linkText}>
                   <Link
@@ -129,64 +139,65 @@ const Navbar = () => {
               <span> Sign in</span>
               <FiArrowRight />
             </a>
-
-            {/* Drawer */}
-            <div className="lg:hidden">
-              <div
-                className="cursor-pointer w-10 h-10 bg-slate-200 dark:bg-zinc-700 hover:bg-slate-300 dark:hover:bg-zinc-800 rounded-lg grid place-items-center"
-                onClick={() => setIsNavbarMenuOpen(!isNavbarMenuOpen)}
-              >
-                <FiMenu />
-              </div>
-              <Drawer
-                size="small"
-                isOpen={isDrawerOpen}
-                onClose={() => setisDrawerOpen(false)}
-              >
-                <ul className="list-none flex flex-col pl-4 component-drawer">
-                  {navbarLinks.map((link) => {
-                    const isActive = pathname === link.url;
-                    return (
-                      <li key={link.linkText}>
-                        <Link
-                          href={link.url}
-                          className={`${isActive ? "text-primary" : ""}`}
-                        >
-                          {link.linkText}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </Drawer>
+            {/* Mobile button toggle */}
+            <div
+              className="cursor-pointer w-10 h-10 bg-slate-200 dark:bg-zinc-700 hover:bg-slate-300 dark:hover:bg-zinc-800 rounded-lg grid place-items-center md:hidden"
+              onClick={() => setIsNavbarMenuOpen(!isNavbarMenuOpen)}
+            >
+              <FiMenu />
             </div>
 
-            <Button
-              buttonRef={commandButtonRef}
-              className="hidden shadow shadow-primary/30 md:flex"
-              variant="outlined"
-              onClick={handleClick}
-              icon={<BiSearchAlt />}
-            >
-              Ctrl + k
-            </Button>
+            {pathname.startsWith("/components") ? (
+              <Button
+                buttonRef={commandButtonRef}
+                className="hidden shadow shadow-primary/30 md:flex"
+                variant="outlined"
+                onClick={handleClick}
+                icon={<BiSearchAlt />}
+              >
+                Ctrl + k
+              </Button>
+            ) : null}
           </div>
         </div>
       </div>
       {/* Mobile Menu */}
       {isNavbarMenuOpen && (
         <motion.ul
-          className="absolute top-10 left-0 w-full p-4 border-b dark:border-b-0 mt-4 md:hidden flex flex-col space-y-2 bg-white dark:bg-zinc-700"
+          className="z-30 absolute top-10 left-0 w-full p-4 border-b dark:border-b-0 mt-4 md:hidden flex flex-col space-y-2 bg-white dark:bg-zinc-700"
           initial="hidden"
           animate="visible"
           exit="hidden"
           variants={dropdownVariants}
         >
-          {navbarLinks.map((link) => (
-            <li key={link.linkText}>
-              <Link href={link.url}>{link.linkText}</Link>
-            </li>
-          ))}
+          {navbarLinks.map((link) => {
+            let isActive = pathname === link.url; // Check for exact match first
+
+            if (!isActive) {
+              if (
+                link.url.startsWith("/components") &&
+                pathname.startsWith("/components")
+              ) {
+                isActive = true; // Check for "/components" and its sub-routes
+              } else if (
+                pathname.startsWith(link.url) &&
+                link.url !== "/components"
+              ) {
+                isActive = true; // Check for other links and their sub-routes
+              }
+            }
+
+            return (
+              <li key={link.linkText}>
+                <Link
+                  href={link.url}
+                  className={`${isActive ? "text-primary" : ""}`}
+                >
+                  {link.linkText}
+                </Link>
+              </li>
+            );
+          })}
         </motion.ul>
       )}
     </nav>
