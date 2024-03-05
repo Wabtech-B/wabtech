f1331beb224c83bd05c837cfce93f23438149d1b
Wabweni Brian <115115387+WabweniBrian@users.noreply.github.com>
2024-03-05 07:24:57 +0300

    Updates

---
+++ app/(main)/blog/tags/[tag]/page.tsx
@@ -0,0 +1,83 @@
+import NoResults from "@/components/no-results";
+import CardOne from "@/components/posts/post-cards/card-one";
+import React from "react";
+import PostPagination from "../../../_components/blog/post-pagination";
+import { Metadata } from "next";
+import SearchInput from "@/app/(main)/_components/blog/search-input";
+import Breadcrumbs from "@/components/breadcrumb";
+
+const getPostsByTag = async (
+  tag: string,
+  { page, limit }: { page: number; limit: number }
+) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/posts/categories/${tag}/posts?page=${page}&limit=${limit}`,
+    { cache: "no-store" }
+  );
+  return res.json();
+};
+
+export function generateMetadata({
+  params,
+}: {
+  params: { tag: string };
+}): Metadata {
+  return {
+    title: decodeURI(params.tag),
+  };
+}
+
+type Params = {
+  searchParams: {
+    page: string;
+    limit: string;
+  };
+  params: { tag: string };
+};
+
+const TagPage = async ({ params, searchParams }: Params) => {
+  const page = Number(searchParams.page) || 1;
+  const limit = Number(searchParams.limit) || 8;
+
+  const { posts, pages }: { posts: TTPost[]; pages: number } =
+    await getPostsByTag(decodeURI(params.tag), {
+      page,
+      limit,
+    });
+
+  return (
+    <div className="max-w-7xl mx-auto px-3 pt-20">
+      <div className="my-2 flex justify-end">
+        <Breadcrumbs separator="/" />
+      </div>
+      <div className="flex-center-between flex-col sm:flex-row gap-3">
+        <h1 className="text-xl md:text-3xl font-bold mt-3">
+          {decodeURI(params.tag)}
+        </h1>
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+        {posts.length > 0 && <PostPagination pages={pages} />}
+      </div>
+      <div className="md:hidden mt-3">
+        <SearchInput />
+      </div>
+      <div className="mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {posts.map((post) => (
+          <CardOne
+            key={post.id}
+            post={post}
+            showAuthor={false}
+            showTags={false}
+            showCommentCount={false}
+            showLikes={false}
+            showDescription={true}
+          />
+        ))}
+      </div>
+      {posts.length < 1 && <NoResults title="Posts Found" />}
+    </div>
+  );
+};
+
+export default TagPage;
