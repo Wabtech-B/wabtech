d1aad9abfe219077efbd93a948f4db090ffe0d71
Wabweni Brian <115115387+WabweniBrian@users.noreply.github.com>
2024-03-09 14:27:58 +0300

    Updates

--- app/(main)/_components/template-details/screenshots.tsx
+++ app/(main)/_components/template-details/screenshots.tsx
@@ -14,28 +14,13 @@ const Screenshots = ({ screenshots }: { screenshots: Screenshot[] }) => {
           data-aos-delay={`${200 * (index / 4)}`}
         >
           <Image
-            src={`/images/temp (1).jpg`}
+            src={screenshot.image}
             alt={screenshot.url}
             fill
             className="object-cover group-hover:scale-105 transition-a"
           />
         </a>
       ))}
-      <a
-        href={`#`}
-        className="w-full shrink-0 h-[200px] md:h-[300px] relative overflow-hidden rounded-xl box-shadow group"
-        target="_blank"
-        referrerPolicy="no-referrer"
-        data-aos="fade-up"
-        data-aos-delay={"250"}
-      >
-        <Image
-          src={`/images/temp (2).jpg`}
-          alt="Alt text"
-          fill
-          className="object-cover group-hover:scale-105 transition-a"
-        />
-      </a>
     </div>
   );
 };
